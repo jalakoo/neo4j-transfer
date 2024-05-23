219294d620c1ac08e81414d2500dc40a0b405fe2
jalakoo <jalakoo@gmail.com>
2024-05-23 15:54:27 -0700

    Default transfer with no config

--- neo4j_transfer/__init__.py
+++ neo4j_transfer/__init__.py
@@ -1,7 +1,6 @@
-from neo4j_transfer.models import Neo4jCredentials, TransferSpec
+from neo4j_transfer.models import Neo4jCredentials, TransferSpec, KeyTransferSpec
 from neo4j_transfer.n4j import validate_credentials, execute_query
 from neo4j_transfer._logger import logger
-import neo4j_transfer.errors as errors_
 from neo4j_uploader import batch_upload
 from neo4j_uploader.models import (
     Relationships,
@@ -10,7 +9,6 @@ from neo4j_uploader.models import (
     Neo4jConfig,
     GraphData,
 )
-from datetime import datetime
 
 
 def transfer(
@@ -27,18 +25,33 @@ def transfer(
         A neo4j_uploader.models.UploadResult object with details of the upload
     """
 
+    logger.info(f"Validating source database creds...")
     validate_credentials(source_creds)
+    logger.info(f"Validating target database creds...")
     validate_credentials(target_creds)
 
+    logger.info(f'Getting nodes from source database "{source_creds.uri}"...')
     nodes = get_nodes(source_creds, spec)
-    rels = get_relationships(source_creds, spec)
+    logger.info(f"Node Specs for upload: {nodes}")
 
+    logger.info(f'Getting relationships from source database "{source_creds.uri}"...')
+    rels = get_relationships(source_creds, spec)
+    logger.info(f"Relationship Specs for upload: {rels}")
+
+    logger.info(f'Uploading data to target database "{target_creds.uri}"...')
     result = upload(target_creds, nodes, rels)
 
     return result
 
 
 def undo(creds: Neo4jCredentials, spec: TransferSpec):
+    """
+    Undo a transfer by deleting all nodes and relationships by timestamp specified in a TransferSpec.
+
+    Args:
+        creds (Neo4jCredentials): Credentials for the target Neo4j instance
+        spec (TransferSpec): Specification used for the data transfer to undo
+    """
 
     timestamp_key = spec.timestamp_key
     ds_string = f"{spec.timestamp.isoformat()}"
@@ -77,15 +90,17 @@ def get_nodes(creds: Neo4jCredentials, spec: TransferSpec) -> list[Nodes]:
     result = []
     for label in spec.node_labels:
 
+        logger.debug(f"Getting {label} nodes...")
+
         nodes_query = f"""
             MATCH (n:`{label}`)
             RETURN n
             """
         records, _, _ = execute_query(creds, nodes_query)
 
-        logger.info(f"\n Number of {label} nodes: {len(records)}")
+        logger.debug(f"Number of {label} nodes found: {len(records)}")
         if len(records) > 0:
-            logger.info(f"\n First Node: {records[0]}")
+            logger.debug(f"First Node: {records[0]}")
 
         converted_records = []
         for n in records:
@@ -94,23 +109,40 @@ def get_nodes(creds: Neo4jCredentials, spec: TransferSpec) -> list[Nodes]:
             # Copy to editable dict
             node = dict(**node_raw)
 
-            if spec.should_append_data:
-                # Get original element id if specified in transfer spec
-                element_id = n.values()[0].element_id
+            # Get specifications for transfering node data. Use default if no mapping found
+            node_mapping = spec.node_mappings()
+            if not isinstance(node_mapping, dict):
+                raise ValueError(
+                    f"Node mappings for {label} is not a dictionary. Found {type(node_mapping)}"
+                )
+            key_spec = node_mapping.get(label, KeyTransferSpec())
+            sid_key = key_spec.source
+            tid_key = key_spec.target
 
-                # Add default transfer related data
+            if sid_key == "element_id":
+                # Node element ids are not returned in the data() api
+                node_element_id = n.values()[0]._element_id
+                # KeyTransferSpec can specify what the target key will be (may not be the source key identifier)
+                node.update({tid_key: node_element_id})
+            else:
+                # User specified keys, not using built-in element_ids
+                uid = node.get(sid_key, None)
+                if uid is None:
+                    logger.warning(f"Node has no {sid_key} property. Skipping: {node}")
+                    continue
+                node.update({tid_key: uid})
+
+            # Append optional timestamp separately
+            if spec.timestamp_key is not None:
                 node.update(
                     {
-                        spec.element_id_key: element_id,
                         spec.timestamp_key: spec.timestamp.isoformat(),
                     }
                 )
 
             converted_records.append(node)
 
-        nodes_spec = Nodes(
-            labels=[label], key=spec.element_id_key, records=converted_records
-        )
+        nodes_spec = Nodes(labels=[label], key=tid_key, records=converted_records)
 
         result.append(nodes_spec)
 
@@ -123,57 +155,181 @@ def get_relationships(
     """Retrieve Relationships from source and format for uploading"""
 
     result = []
-    for type in spec.relationship_types:
+    r_types = spec.relationship_mappings().keys()
+    for r_type in r_types:
+        logger.debug(f'Processing relationships of type "{r_type}"...')
+
         query = """
             MATCH (n)-[r]->(n2)
             WHERE any(label IN labels(n) WHERE label IN $labels) AND any(label IN labels(n2) WHERE label IN $labels) AND type(r) in $types
             RETURN n, r, n2
         """
-        params = {"labels": spec.node_labels, "types": [type]}
+        node_labels = list(spec.node_mappings().keys())
+        params = {"labels": node_labels, "types": [r_type]}
         records, _, _ = execute_query(creds, query, params)
 
-        source_node = TargetNode(
-            node_label=None,
-            node_key=spec.element_id_key,
-            record_key=f"_from_{spec.element_id_key}",
-        )
-        target_node = TargetNode(
-            node_label=None,
-            node_key=spec.element_id_key,
-            record_key=f"_to_{spec.element_id_key}",
-        )
+        logger.debug(f"Number of {r_type} relationships found: {len(records)}")
+        # Going to sort in groups of source-targest-relationshipTypes : Relationship objects
+        sorter = {}
+        for r in records:
 
-        converted_records = []
-        for rec in records:
-            from_eid = rec.values()[0].element_id
-            to_eid = rec.values()[2].element_id
-            r_eid = rec.values()[1].element_id
+            n_labels = list(r.values()[0].labels)
+            n2_labels = list(r.values()[2].labels)
+            # rel_type and r_type should be the same
+            if r_type != r.values()[1].type:
+                raise ValueError(
+                    f"Relationship type {r_type} from mappings does not match relationship type {r.values()[1].type} from db records"
+                )
+            # r_type = r.values()[1].type
+            key = f"{n_labels} : {n2_labels} : {r_type}"
 
-            # TODO: Relationship properties
+            # Using only 1 label from each source & target node to key configuration data
+            from_node_label = n_labels[0]
+            to_node_label = n2_labels[0]
 
-            # Required data to connect relationships with source and target nodes
-            rel_rec = {
-                f"_from_{spec.element_id_key}": from_eid,
-                f"_to_{spec.element_id_key}": to_eid,
+            # Getting mapping data for that label
+            from_key_spec = spec.node_mappings().get(from_node_label, None)
+            if from_key_spec is None:
+                raise ValueError(f"No node mapping found for label: {from_node_label}")
+            to_key_spec = spec.node_mappings().get(to_node_label, None)
+            if to_key_spec is None:
+                raise ValueError(f"No node mapping found for label: {to_node_label}")
+
+            # Get source Node keys to use
+            from_key = from_key_spec.source
+            to_key = to_key_spec.source
+
+            # Get the Node keys in the duplicated Nodes data
+            from_transfer_key = from_key_spec.target
+            to_transfer_key = to_key_spec.target
+
+            # Create target node keys for the relationship records only
+            from_target_key = (
+                f"_from_{from_key_spec.target}"
+                if from_key_spec.target is not None
+                else f"_from_{from_key}"
+            )
+            to_target_key = (
+                f"_to_{to_key_spec.target}"
+                if to_key_spec.target is not None
+                else f"_to_{to_key}"
+            )
+
+            # Get node key-values
+            from_node_values = dict(**r.values()[0]._properties)
+            to_node_values = dict(**r.values()[2]._properties)
+            if not isinstance(from_node_values, dict):
+                raise ValueError(
+                    f"Source Node values are not a dictionary. Got: {from_node_values}"
+                )
+            if not isinstance(to_node_values, dict):
+                raise ValueError(
+                    f"Target Node values are not a dictionary. Got: {to_node_values}"
+                )
+
+            # Exception for element id, which exists outside of properties data
+            if from_key == "element_id":
+                from_key_value = r.values()[0]._element_id
+            else:
+                from_key_value = from_node_values.get(from_key, None)
+
+            if to_key == "element_id":
+                to_key_value = r.values()[2]._element_id
+            else:
+                to_key_value = to_node_values.get(to_key, None)
+
+            if from_key_value is None or to_key_value is None:
+                logger.warning(
+                    f"Relationship has no {from_key} or {to_key} property. Skipping: {r}"
+                )
+                continue
+
+            # Create a relationship dictionary
+            record_dict = {
+                from_target_key: from_key_value,
+                to_target_key: to_key_value,
             }
 
-            if spec.should_append_data:
-                # Add default transfer related data
-                rel_rec.update(
+            if spec.timestamp_key is not None:
+                record_dict.update(
                     {
-                        spec.element_id_key: r_eid,
                         spec.timestamp_key: spec.timestamp.isoformat(),
                     }
                 )
-            converted_records.append(rel_rec)
 
-        rels_spec = Relationships(
-            type=type,
-            from_node=source_node,
-            to_node=target_node,
-            records=converted_records,
-        )
-        result.append(rels_spec)
+            if key in sorter:
+                # Append new data to existing Relationships record
+                sorter[key].records.append(record_dict)
+            else:
+                # Create a new Relationships record
+                source_node = TargetNode(
+                    node_label=from_node_label,
+                    node_key=from_transfer_key,
+                    record_key=from_target_key,
+                )
+                target_node = TargetNode(
+                    node_label=to_node_label,
+                    node_key=to_transfer_key,
+                    record_key=to_target_key,
+                )
+                sorter[key] = Relationships(
+                    type=r_type,
+                    from_node=source_node,
+                    to_node=target_node,
+                    records=[record_dict],
+                )
+
+        logger.debug(f"Sorted relationship combinations: {sorter.keys()}")
+
+        # Convert to list of Relationship objects
+        for value in sorter.values():
+            # Each record will now be a Neo4jUploader Relationships Object
+            result.append(value)
+
+        logger.debug(f"Relationship Specifications generated: {len(result)}")
+
+        # source_node = TargetNode(
+        #     node_label=None,
+        #     node_key=spec.element_id_key,
+        #     record_key=f"_from_{spec.element_id_key}",
+        # )
+        # target_node = TargetNode(
+        #     node_label=None,
+        #     node_key=spec.element_id_key,
+        #     record_key=f"_to_{spec.element_id_key}",
+        # )
+
+        # converted_records = []
+        # for rec in records:
+        #     from_eid = rec.values()[0].element_id
+        #     to_eid = rec.values()[2].element_id
+        #     r_eid = rec.values()[1].element_id
+
+        #     # TODO: Relationship properties
+
+        #     # Required data to connect relationships with source and target nodes
+        #     rel_rec = {
+        #         f"_from_{spec.element_id_key}": from_eid,
+        #         f"_to_{spec.element_id_key}": to_eid,
+        #     }
+
+        #     if spec.should_append_data:
+        #         # Add default transfer related data
+        #         rel_rec.update(
+        #             {
+        #                 spec.element_id_key: r_eid,
+        #                 spec.timestamp_key: spec.timestamp.isoformat(),
+        #             }
+        #         )
+        #     converted_records.append(rel_rec)
+
+        # rels_spec = Relationships(
+        #     type=type,
+        #     from_node=source_node,
+        #     to_node=target_node,
+        #     records=converted_records,
+        # )
+        # result.append(rels_spec)
 
     return result
 
--- neo4j_transfer/models.py
+++ neo4j_transfer/models.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from pydantic import BaseModel, Field
 from typing import Optional
 from datetime import datetime
@@ -37,6 +38,19 @@ def generate_id():
     return binascii.hexlify(os.urandom(8)).decode()
 
 
+class KeyTransferSpec(BaseModel):
+    """Configuration for transferring Nodes between Neo4j instances
+
+
+    Args:
+        source (str, optional): The property key to use from the source data to use as the Node id. Defaults to "element_id".
+        target (str, optional): The property key to use from the target data to use as the Node id. If None, the source key will be copied over to the target node. Defaults to None.
+    """
+
+    source: Optional[str] = "element_id"
+    target: Optional[str] = "_transfer_element_id"
+
+
 class TransferSpec(BaseModel):
     """Configuraiton for transferring data between Neo4j instances
 
@@ -49,18 +63,91 @@ class TransferSpec(BaseModel):
 
         element_id_key (str): The copy of the source Node/Relationship's element id value will be inserted as the value of this property key to the duplicated Nodes/Relationships. Defaults to "_transfer_element_id".
 
-        timestamp_key (str): The timestamp key identifying when the transfer was initiated will be inserted as the value of this property key to the duplicated Nodes/Relationships. Defaults to "_transfer_timestamp".
+        timestamp_key (str): The timestamp key identifying when the transfer was initiated will be inserted as the value of this property key to the duplicated Nodes/Relationships. Defaults to "_transfer_timestamp". If set to None, no timestamp to copied data will be added. This will prevent a subsequent undo() call from working.
 
         timestamp (datetime): Timestamp of transfer. Defaults to datetime when the TransferSpec object created.
 
     """
 
-    node_labels: list[str]
-    relationship_types: Optional[list[str]] = []
-    should_append_data: bool = True
-    element_id_key: str = "_transfer_element_id"
     timestamp_key: str = "_transfer_timestamp"
     timestamp: datetime = Field(default_factory=datetime.now)
+
+    node_labels: list[str] | dict[str, KeyTransferSpec] = Field(default_factory=list)
+    relationship_types: list[str] | dict[str, KeyTransferSpec] = Field(
+        default_factory=list
+    )
+
+    def node_mappings(self) -> dict:
+        """This function will convert a string list of Node labels to a dictionary of label: KeyTransferSpec. If a dictionary was originally assigned to the node_labels property, this function will return that dictionary unchanged.
+
+        Raises:
+            Exception: Raises ValueError if the node_labels property is not a list of strings or a dictionary of type string: KeyTransferSpec.
+
+        Returns:
+            dict: Dictionary of Node labels and their corresponding KeyTransferSpec.
+        """
+        result = {}
+        if isinstance(self.node_labels, list):
+            for label in self.node_labels:
+                if not isinstance(label, str):
+                    raise ValueError(f"Node label '{label}' is not of type string")
+                # Use default KeyTransferSpec if only Node label and Relationship Types specified
+                result[label] = KeyTransferSpec()
+        elif isinstance(self.node_labels, dict):
+            # Check dict of string: KeyTransferSpec
+            for key, value in self.node_labels.items():
+                if isinstance(value, KeyTransferSpec):
+                    result[key] = value
+                else:
+                    try:
+                        value = KeyTransferSpec(**value)
+                        result[key] = value
+                    except Exception as e:
+                        raise Exception(
+                            f"Could not convert value for key '{key}' to a KeyTransferSpec. ERROR: {e}"
+                        )
+        else:
+            raise Exception(
+                "Invalid node_labels type. Expect list of strings or dictionary of string: KeyTransferSpec"
+            )
+        return result
+
+    def relationship_mappings(self) -> dict:
+        """Returns a dictionary of Relationship types and their corresponding KeyTransferSpec. If a dictionary was originally assigned to the relationship_types property, this function will return that dictionary unchanged.
+
+        Raises:
+            Exception: Raises ValueError if the relationship_types property is not a list of strings or a dictionary of type string: KeyTransferSpec.
+
+        Returns:
+            dict: Dictionary of Relationship types and their corresponding KeyTransferSpec.
+        """
+        result = {}
+
+        if isinstance(self.relationship_types, list):
+            # Check is list of strings
+            if not all(isinstance(x, str) for x in self.relationship_types):
+                raise ValueError("relationship_types must be a list of strings")
+            for rel_type in self.relationship_types:
+                result[rel_type] = KeyTransferSpec()
+        elif isinstance(self.relationship_types, dict):
+            # Check dict of string: KeyTransferSpec
+            for key, value in self.relationship_types.items():
+                if isinstance(value, KeyTransferSpec):
+                    result[key] = value
+                else:
+                    try:
+                        value = KeyTransferSpec(**value)
+                        result[key] = value
+                    except Exception as e:
+                        raise Exception(
+                            f"Could not convert value for key '{key}' to a KeyTransferSpec. ERROR: {e}"
+                        )
+        else:
+            raise Exception(
+                "Invalid relationship_types type. Expect list of strings or dictionary of string: KeyTransferSpec"
+            )
+
+        return result
 
     def __hash__(self):
         return hash((type(self),) + tuple(self.dict().items()))
